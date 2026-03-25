e68dd5bda99c97957a8acef7f69131a87cd5930f
Mateusz Czarnik <matysz21@wp.pl>
2026-03-25 12:41:36 +0100

    Initial commit: Setup Lacco project on master branch

---
+++ Backend/src/main/java/com/example/Lacco/LaccoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lacco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaccoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaccoApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/Lacco/LaccoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lacco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaccoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
